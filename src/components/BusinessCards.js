7905e18b623b86048c6ec1f5596519edd49fcced
moseley <jeremymoseley@me.com>
2020-03-30 07:17:02 -0700

    Remove Axios Response import

--- src/components/BusinessCards.js
+++ src/components/BusinessCards.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
