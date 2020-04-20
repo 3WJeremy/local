7a2c3aeee16be729b9b67812ffb7434632559a56
moseley (via Travis CI) <jeremymoseley@me.com>
2020-04-20 14:26:25 +0000

    Deploy 3WJeremy/local to github.com/3WJeremy/local.git:gh-pages

---
+++ precache-manifest.0114664ceeba4915e4192c0467c17a36.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3d9a4aec6d386a0ccba9e6c89520f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "381e8fd23a97a1518317",
+    "url": "/static/css/2.47271137.chunk.css"
+  },
+  {
+    "revision": "381e8fd23a97a1518317",
+    "url": "/static/js/2.1ca38d2a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.1ca38d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a1a115d27d6a60a32b",
+    "url": "/static/js/main.1051824d.chunk.js"
+  },
+  {
+    "revision": "756a215a2c87ed241740",
+    "url": "/static/js/runtime-main.d5b173d6.js"
+  },
+  {
+    "revision": "9dc48323fbbcdd4864862059629c3bd2",
+    "url": "/static/media/activities.9dc48323.png"
+  },
+  {
+    "revision": "cc789b4e1b59f260f35180485c7f2240",
+    "url": "/static/media/advanced-audiology.cc789b4e.png"
+  },
+  {
+    "revision": "87fc87f935d3b4eeb7577194fade32ab",
+    "url": "/static/media/advisory-group-west.87fc87f9.png"
+  },
+  {
+    "revision": "5ae700e77adfcd8691aa447a764ad6eb",
+    "url": "/static/media/audiology-associates.5ae700e7.png"
+  },
+  {
+    "revision": "48de2a320523a6e25d46f815395fc21c",
+    "url": "/static/media/automotive.48de2a32.png"
+  },
+  {
+    "revision": "40779d6c2ea7ffb3a192cce5d879c13b",
+    "url": "/static/media/banking.40779d6c.png"
+  },
+  {
+    "revision": "05ec7ea632942592fb098b16fc2cae5f",
+    "url": "/static/media/baywood-dental-group.05ec7ea6.png"
+  },
+  {
+    "revision": "041982b4ddb1d70831b37c8addee8404",
+    "url": "/static/media/beauty.041982b4.png"
+  },
+  {
+    "revision": "acdfbf172c95a55fcdf7435140a910c3",
+    "url": "/static/media/comfort-keepers.acdfbf17.png"
+  },
+  {
+    "revision": "d887cf78acb5c5f1e81075e1de3355da",
+    "url": "/static/media/compass.d887cf78.png"
+  },
+  {
+    "revision": "75696050f2e9aca44b9de4cf8145eae2",
+    "url": "/static/media/ecola-termite-pest-control.75696050.png"
+  },
+  {
+    "revision": "eccd7b97b7fae3a9d5e94f12da79aca6",
+    "url": "/static/media/education.eccd7b97.png"
+  },
+  {
+    "revision": "ef7a885590bf55656c96f97d4e31fe53",
+    "url": "/static/media/entertainment.ef7a8855.png"
+  },
+  {
+    "revision": "cd048130fe84c9d2a2e22ad9f7ae5b01",
+    "url": "/static/media/estate-planning.cd048130.png"
+  },
+  {
+    "revision": "14939c9f240f45e41e8784301b7737e0",
+    "url": "/static/media/helm-vision-group.14939c9f.png"
+  },
+  {
+    "revision": "6edb8e79c536584d05f1f9c75f9edba3",
+    "url": "/static/media/home-improvement.6edb8e79.png"
+  },
+  {
+    "revision": "79dbced2f48012a47e519f0cc7b9b69a",
+    "url": "/static/media/investing.79dbced2.png"
+  },
+  {
+    "revision": "d4ce02855ede4b2f3a28b4a5b315e4bf",
+    "url": "/static/media/investors-business-daily.d4ce0285.png"
+  },
+  {
+    "revision": "75f2d637907b5f9095f17231aeb04ea1",
+    "url": "/static/media/jorge-perazzo.75f2d637.png"
+  },
+  {
+    "revision": "149d22526fdc1d3643f66238892e6eb3",
+    "url": "/static/media/legal-services.149d2252.png"
+  },
+  {
+    "revision": "3978c8f95533c09caa415b4eb3390db5",
+    "url": "/static/media/logo.3978c8f9.png"
+  },
+  {
+    "revision": "1c7641134f9c6d70eb1347657c3b1b00",
+    "url": "/static/media/medical.1c764113.png"
+  },
+  {
+    "revision": "ad7dbefa66cbddf78e2181ca1b11656b",
+    "url": "/static/media/millennium-dental.ad7dbefa.png"
+  },
+  {
+    "revision": "4f82e7a1438ca91d877b8e1b9c0fdd20",
+    "url": "/static/media/my-shoes-my-journey.4f82e7a1.png"
+  },
+  {
+    "revision": "7edf57a8dc287ad558292c4fa32d931c",
+    "url": "/static/media/pets.7edf57a8.png"
+  },
+  {
+    "revision": "d293936c5d589bc2e564a0d534149a02",
+    "url": "/static/media/professional-services.d293936c.png"
+  },
+  {
+    "revision": "36529db350be0d09d3bf6b5ebb38fcdf",
+    "url": "/static/media/progressive-insulation-windows.36529db3.png"
+  },
+  {
+    "revision": "27949c5eadffc6fc5fb0898411abd71e",
+    "url": "/static/media/retirement.27949c5e.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "289ae85ad3bc69ab171b52aef50270d9",
+    "url": "/static/media/total-financial-solutions.289ae85a.png"
+  },
+  {
+    "revision": "6588aed34c57e40af70968f656865b54",
+    "url": "/static/media/travel.6588aed3.png"
+  },
+  {
+    "revision": "b41f0f61b15f8da34237c9798f2c41a8",
+    "url": "/static/media/tyra-beavers.b41f0f61.png"
+  },
+  {
+    "revision": "77798dd6f334938d4b93c2cea570f6dc",
+    "url": "/static/media/wells-fargo.77798dd6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/local/precache-manifest.0d5d089bebc2f86834989023c569692c.js"
+  "/precache-manifest.0114664ceeba4915e4192c0467c17a36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/local/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
